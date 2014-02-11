4ff9a3d426126afaaa52fae63e29ded7519102f3
Luis Gerhorst <luisgerhorst@imac-215.speedport_w_921v_1_24_000>
2014-02-11 15:25:03 +0100

    Initial Commit

---
+++ LV Konverter/LGDocument.h
@@ -0,0 +1,13 @@
+//
+//  LGDocument.h
+//  LV Konverter
+//
+//  Created by Luis Gerhorst on 11.02.14.
+//  Copyright (c) 2014 Luis Gerhorst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LGDocument : NSDocument
+
+@end
---
+++ LV KonverterTests/LV_KonverterTests.m
@@ -0,0 +1,34 @@
+//
+//  LV_KonverterTests.m
+//  LV KonverterTests
+//
+//  Created by Luis Gerhorst on 11.02.14.
+//  Copyright (c) 2014 Luis Gerhorst. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LV_KonverterTests : XCTestCase
+
+@end
+
+@implementation LV_KonverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
