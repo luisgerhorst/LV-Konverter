adb6e8ebe8646653bd3e9e2a358d260fee0613c1
Luis Gerhorst <luis@luisgerhorst.de>
2014-07-19 16:08:28 +0200

    do not open untitled document on launch

---
+++ LV Konverter/LGApplicationDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LGApplicationDelegate.h
+//  LV Konverter
+//
+//  Created by Luis Gerhorst on 19.07.14.
+//  Copyright (c) 2014 Luis Gerhorst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LGApplicationDelegate : NSObject<NSApplicationDelegate>
+
+@end
---
+++ LV Konverter/LGApplicationDelegate.m
@@ -0,0 +1,18 @@
+//
+//  LGApplicationDelegate.m
+//  LV Konverter
+//
+//  Created by Luis Gerhorst on 19.07.14.
+//  Copyright (c) 2014 Luis Gerhorst. All rights reserved.
+//
+
+#import "LGApplicationDelegate.h"
+
+@implementation LGApplicationDelegate
+
+- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)sender
+{
+    return NO;
+}
+
+@end
